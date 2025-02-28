1d10031783845fb7ea06a5123b4178a4b4619d1f
PaulineDuarte <Paulineduarte1@hotmail.com>
2025-02-28 11:24:29 -0300

    lista dos horarios

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,13 +1,22 @@
 import {openingHours} from "../../utils/opening-hours"
 import dayjs from "dayjs"
 
-export function hoursLoad (date) {
+export function hoursLoad ({date}) {
 
     const opening = openingHours.map((hour) => {
         //recuperar somente a hora 
         const [scheduleHour] = hour.split(":")
 
         //Verificar se a hora está no passado 
-        const isHourPast = dayjs(date).add(scheduleHour,"hour").isBefore(dayjs())
+        const isHourPast = dayjs(date).add(scheduleHour,"hour").isAfter(dayjs())
+
+        //define se o horario está disponivel 
+        return {
+            hour, 
+            availabe : isHourPast
+        }
+
     })
+
+    
 }
