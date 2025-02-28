d3da5c433ccfdca2f75d840cacc06c1aa67f7e7a
PaulineDuarte <Paulineduarte1@hotmail.com>
2025-02-28 20:51:12 -0300

    Renderizando os horarios

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import {openingHours} from "../../utils/opening-hours"
 import dayjs from "dayjs"
 
+const hours = document.getElementById("hours")
+
 export function hoursLoad ({date}) {
 
     const opening = openingHours.map((hour) => {
@@ -18,5 +20,15 @@ export function hoursLoad ({date}) {
 
     })
 
-    
+    // Renderizar os horarios
+    opening.forEach(({hour,availabe}) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(availabe ? "hour-available" : "hour-unavailable" )
+
+        li.textContent = hour
+
+        hours.appendChild(li)
+    })
 }
