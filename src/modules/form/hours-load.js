98405671cbd734620247c9fa169782a99e72245a
PaulineDuarte <Paulineduarte1@hotmail.com>
2025-02-28 11:04:39 -0300

    Carregando os horários

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,13 @@
+import {openingHours} from "../../utils/opening-hours"
+import dayjs from "dayjs"
+
+export function hoursLoad (date) {
+
+    const opening = openingHours.map((hour) => {
+        //recuperar somente a hora 
+        const [scheduleHour] = hour.split(":")
+
+        //Verificar se a hora está no passado 
+        const isHourPast = dayjs(date).add(scheduleHour,"hour").isBefore(dayjs())
+    })
+}
