9f923a6f9581310bbba66dba1804f8603cf35276
PaulineDuarte <Paulineduarte1@hotmail.com>
2025-03-04 11:12:44 -0300

    Validar campos e inserir hora na data selecionada

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs"
 
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Data atual para formatar o  input 
@@ -15,4 +16,41 @@ selectedDate.min = inputToday
 
 form.onsubmit = (e) => {
     e.preventDefault()
+
+
+    try {
+        //recuperando o nome do cliente 
+        const name = clientName.value.trim()
+
+        if(!name){
+            return alert("Informe o nome do cliente")
+        }
+
+        // recuperar o horário selecionado
+        const hourSelected = document.querySelector(".hour-selected")
+
+        // recuperar o horario selecionado
+        if(!hourSelected){
+            return alert("Selecione a hora")
+        }
+
+        //Recuperar somente a hora
+        const [hour] = hourSelected.innerText.split(":")
+
+        //Insere a hora na data
+        const when = dayjs(selectedDate.value).add(hour,"hour")
+
+        //Gera um ID
+        const id = new Date().getTime()
+
+        console.log ({
+            id,
+            name,
+            when
+        })
+        
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento")
+        console.log(error)
+    }
 }
