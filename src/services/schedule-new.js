c180d7327fecedc7e7dfe44362dafa444d3d02b4
PaulineDuarte <Paulineduarte1@hotmail.com>
2025-03-07 12:12:31 -0300

    update description

--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -3,6 +3,7 @@ import { apiConfig } from "./api-config.js"
 
 export async function ScheduleNew({id,name,when}) {
     try {
+        //  Faz a requisição para enviar os dados do agendamento.
         await fetch(`${apiConfig.baseURL}/schedules`,{
                 method : "POST",
                 headers : {
